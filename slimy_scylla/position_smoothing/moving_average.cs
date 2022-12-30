9679307752bf0632749dd2fcb12e8484339c8eea
kuuube <hexagonisalie@gmail.com>
2022-12-30 15:08:17 -0500

    Add non-catch up moving average

--- slimy_scylla/position_smoothing/moving_average.cs
+++ slimy_scylla/position_smoothing/moving_average.cs
@@ -7,12 +7,13 @@ using OpenTabletDriver.Plugin.Output;
 namespace slimy_scylla;
 
 [PluginName("Slimy Scylla Position Smoothing Moving Average")]
-public sealed class slimy_scylla_position_smoothing_moving_average : IPositionedPipelineElement<IDeviceReport>
+public sealed class slimy_scylla_position_smoothing_moving_average : slimy_scylla_base
 {
     private bool emit = true;
     private List<Vector2> last_positions = new List<Vector2>();
+    private Vector2 last_smoothed_position = new Vector2();
 
-    private Vector2 moving_average_catch_up(Vector2 report) {
+    private Vector2 moving_average(Vector2 report) {
         last_positions.Add(report);
         while (last_positions.Count > amount) {
             last_positions.RemoveAt(0);
@@ -34,12 +35,8 @@ public sealed class slimy_scylla_position_smoothing_moving_average : IPositioned
         return report;
     }
 
-    private Vector2 moving_average(Vector2 report) {
-        return report;
-    }
-
-    public event Action<IDeviceReport> Emit;
-    public void Consume(IDeviceReport device_report)
+    public override event Action<IDeviceReport> Emit;
+    public override void Consume(IDeviceReport device_report)
     {
         if (device_report is ITabletReport report) {
             if (!apply_to_hover && report.Pressure <= pressure_deadzone) {
@@ -49,9 +46,25 @@ public sealed class slimy_scylla_position_smoothing_moving_average : IPositioned
             }
 
             if (catch_up) {
-                report.Position = moving_average_catch_up(report.Position);
-            } else {
                 report.Position = moving_average(report.Position);
+            } else {
+                //ignore reports that move less than one pixel
+                float max_lppx = Math.Max(lines_per_pixel().X, lines_per_pixel().Y);
+                float max_delta;
+                if (last_positions.Count > 0) {
+                    max_delta = Math.Max(Math.Abs(report.Position.X - last_positions.Last().X), Math.Abs(report.Position.Y - last_positions.Last().Y));
+                } else {
+                    max_delta = Math.Max(report.Position.X, report.Position.Y);
+                }
+
+                if (max_delta < max_lppx) {
+                    report.Position = last_smoothed_position;
+                    device_report = report;
+                    Emit?.Invoke(device_report);
+                    return;
+                }
+                report.Position = moving_average(report.Position);
+                last_smoothed_position = report.Position;
             }
             device_report = report;
         }
@@ -62,12 +75,12 @@ public sealed class slimy_scylla_position_smoothing_moving_average : IPositioned
             emit = true;
         }
     }
-    public PipelinePosition Position => PipelinePosition.PreTransform;
+    public override PipelinePosition Position => PipelinePosition.PreTransform;
 
     [Property("Amount"), DefaultPropertyValue(2)]
     public int amount { set; get; }
 
-    [BooleanProperty("Catch Up", "")]
+    [BooleanProperty("Catch Up (pen only)", "")]
     public bool catch_up { set; get; }
 
     [Property("Pressure Deadzone")]
