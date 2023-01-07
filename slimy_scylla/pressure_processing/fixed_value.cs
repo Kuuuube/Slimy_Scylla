b4279a6bc0f702fe76a59e3cd3c1c9c19d78ec10
kuuube <hexagonisalie@gmail.com>
2023-01-07 00:00:31 -0500

    Add Remove Tail Pressure Reports to Fixed Value and Sample and Hold pressure processing

--- slimy_scylla/pressure_processing/fixed_value.cs
+++ slimy_scylla/pressure_processing/fixed_value.cs
@@ -9,17 +9,26 @@ namespace slimy_scylla;
 [PluginName("Slimy Scylla Pressure Processing Fixed Value")]
 public sealed class slimy_scylla_pressure_processing_fixed_value : slimy_scylla_base
 {
+    private Vector2 last_pos = new Vector2();
+    private int tail_reports = 0;
+
     public override event Action<IDeviceReport>? Emit;
     public override void Consume(IDeviceReport device_report)
     {
         if (device_report is ITabletReport report) {
             if (report.Pressure <= pressure_deadzone_percent / 100 * get_max_pressure()) {
+                if (tail_reports > 0) {
+                    report.Position = last_pos;
+                    tail_reports--;
+                }
                 report.Pressure = 0;
                 Emit?.Invoke(device_report);
                 return;
             }
 
             report.Pressure = (uint)(value_percent * get_max_pressure());
+            last_pos = report.Position;
+            tail_reports = remove_tail_pressure_reports;
             device_report = report;
         }
 
@@ -36,4 +45,10 @@ public sealed class slimy_scylla_pressure_processing_fixed_value : slimy_scylla_
         ("Pressure Deadzone: Min: 0%, Max: 100%, Default: 0%\n" +
         "Adds a pressure deadzone at the set pressure percent. Match this value to your Tip Threshold in the Pen Settings tab.")]
     public float pressure_deadzone_percent { set; get; }
+
+    [Property("Remove Tail Pressure Reports"), DefaultPropertyValue(1), ToolTip
+        ("Remove Tail Pressure Reports: Min: 0, Max: 10, Default: 1\n" +
+        "Stops drawing programs from adding their own smoothing at the end of lines which commonly creates \"shoelace line endings\" or \"line tails\".\n" +
+        "Usually setting this to 1 is enough for it function properly. Only increase the value if required.")]
+    public int remove_tail_pressure_reports { set; get; }
 }
--- slimy_scylla/pressure_processing/sample_and_hold.cs
+++ slimy_scylla/pressure_processing/sample_and_hold.cs
@@ -11,6 +11,10 @@ public sealed class slimy_scylla_pressure_processing_sample_and_hold : slimy_scy
 {
     private uint hold_pressure = 0;
     private int samples = 0;
+    private Vector2 last_pos = new Vector2();
+    private int tail_reports = 0;
+
+
     public override event Action<IDeviceReport>? Emit;
     public override void Consume(IDeviceReport device_report)
     {
@@ -18,8 +22,12 @@ public sealed class slimy_scylla_pressure_processing_sample_and_hold : slimy_scy
             samples++;
 
             if (report.Pressure <= pressure_deadzone_percent / 100 * get_max_pressure()) {
-                report.Pressure = 0;
+                if (tail_reports > 0) {
+                    report.Position = last_pos;
+                    tail_reports--;
+                }
                 samples = 0;
+                report.Pressure = 0;
                 Emit?.Invoke(device_report);
                 return;
             }
@@ -32,6 +40,8 @@ public sealed class slimy_scylla_pressure_processing_sample_and_hold : slimy_scy
                 report.Pressure = hold_pressure;
             }
 
+            last_pos = report.Position;
+            tail_reports = remove_tail_pressure_reports;
             device_report = report;
         }
 
@@ -48,4 +58,10 @@ public sealed class slimy_scylla_pressure_processing_sample_and_hold : slimy_scy
         ("Pressure Deadzone: Min: 0%, Max: 100%, Default: 0%\n" +
         "Adds a pressure deadzone at the set pressure percent. Match this value to your Tip Threshold in the Pen Settings tab.")]
     public float pressure_deadzone_percent { set; get; }
+
+    [Property("Remove Tail Pressure Reports"), DefaultPropertyValue(1), ToolTip
+        ("Remove Tail Pressure Reports: Min: 0, Max: 10, Default: 1\n" +
+        "Stops drawing programs from adding their own smoothing at the end of lines which commonly creates \"shoelace line endings\" or \"line tails\".\n" +
+        "Usually setting this to 1 is enough for it function properly. Only increase the value if required.")]
+    public int remove_tail_pressure_reports { set; get; }
 }
